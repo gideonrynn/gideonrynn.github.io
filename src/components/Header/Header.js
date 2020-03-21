8a4f4aa1df6a32f88969d404b9ecc77d4b15288a
gideonrynn <sarah.c.dillard@gmail.com>
2020-03-21 16:24:08 -0500

    updated header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Header = () => {
+export const Header = () => {
     return (
         <div>
-            
+            Hello World!
         </div>
     )
 }
-
-export default Header
